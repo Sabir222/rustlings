401567d1e225b59a40515586642eb426f37bcbdf
sabir222 <mrsabir4@gmail.com>
2025-01-23 20:31:45 +0100

    adding: quiz2,hashmaps,modules

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,23 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut result = Vec::new();
+        for (mut string, command) in input {
+            let output = match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append(n) => {
+                    for _ in 0..n {
+                        string.push_str("bar");
+                    }
+                    string
+                }
+            };
+            result.push(output);
+        }
+        result
+    }
 }
 
 fn main() {
@@ -39,7 +55,7 @@ mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
     use super::Command;
-
+    use crate::my_module::transformer;
     #[test]
     fn it_works() {
         let input = vec![
